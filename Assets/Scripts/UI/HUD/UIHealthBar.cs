5b19b1236fd1536bbdbcf28cb62929eadbf1aecf
David Zack Walker <davidzzwalker@gmail.com>
2019-07-28 15:10:52 +0200

    Fixed healthbar issue
    - fixed issue where healthbar would move out of screen when player was damaged
    - fixed issue where healthbar would turn white when player was damaged
    - added changable healthbar colors

--- Assets/Scripts/UI/HUD/UIHealthBar.cs
+++ Assets/Scripts/UI/HUD/UIHealthBar.cs
@@ -9,6 +9,10 @@ public class UIHealthBar : MonoBehaviour
     private float _midHealthThreshold;
     private float _lowHealthThreshold;
 
+    public Color highHealthColor;
+    public Color midHealthColor;
+    public Color lowHealthColor;
+
     public static UIHealthBar Instance
     {
         get;
@@ -28,6 +32,7 @@ public class UIHealthBar : MonoBehaviour
         _originalWidth = mask.rectTransform.rect.width;
         _midHealthThreshold = _originalWidth / 2;
         _lowHealthThreshold = _originalWidth / 5;
+        SetColor(_originalWidth);
     }
 
     public void SetValue(float value)
@@ -41,11 +46,11 @@ public class UIHealthBar : MonoBehaviour
     {
         var image = GetComponent<Image>();
         if (width >= _midHealthThreshold)
-            image.color = Color.white;
+            image.color = highHealthColor;
         else if (width <= _midHealthThreshold && width > _lowHealthThreshold)
-            image.color = Color.yellow;
+            image.color = midHealthColor;
         else
-            image.color = Color.red;
+            image.color = lowHealthColor;
     }
 
     // Update is called once per frame
