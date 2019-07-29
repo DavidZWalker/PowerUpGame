31d8d07c68d7f90e9a07e9a1ff7b35299ca4034b
David Zack Walker <davidzzwalker@gmail.com>
2019-07-29 18:07:00 +0200

    added more upgrades

---
+++ Assets/Scripts/Collectables/WeaponUpgrades/MagSizeUpgrade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagSizeUpgrade : WeaponUpgradeBase
+{
+    public override void Upgrade(StandardGun weapon)
+    {
+        weapon.magSize++;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
